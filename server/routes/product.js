b50bb84f84cfb1c57c35aa4581876cb2d559540e
yeonlisa <tkzw4869@gmail.com>
2022-02-17 15:39:25 +0900

    multer를 이용하여 이미지 저장

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,12 +2,37 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.jpg' || ext !== '.png') {
+            return cb(res.status(400).end('only jpg, png are allowed'), false);
+        }
+        cb(null, true)
+    }
+})
+
+var upload = multer({ storage: storage }).single("file")
+
 //=================================
 //             Product
 //=================================
 
 router.post("/image", (req, res) => {
     // 가져온 이미지를 저장을 해준다.
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
+    })
 });
 
 
