f04a834ed59afa82180da893fdfca5be2f4dd3e7
yeonlisa <tkzw4869@gmail.com>
2022-02-17 15:16:06 +0900

    onChange Event 처리하기

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,81 @@
-import React from 'react'
+/*eslint-disable*/
+import React, { useState } from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
 
 function UploadProductPage() {
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
+    const [Images, setImages] = useState([])
+
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
+    }
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
+    }
+
+
     return (
-        <div>
-            UploadProductPage
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}>여행 상품 업로드</Title>
+            </div>
+
+
+            <Form onSubmit={onSubmit} >
+
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
+
+                <br />
+                <br />
+                <label>이름</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={TitleValue}
+                />
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={DescriptionValue}
+                />
+                <br />
+                <br />
+                <label>가격($)</label>
+                <Input
+                    onChange={onPriceChange}
+                    value={PriceValue}
+                    type="number"
+                />
+                <br /><br />
+                <select>
+                    <option></option>
+                </select>
+                <br />
+                <br />
+
+                <Button
+                    onClick={onSubmit}
+                >
+                    확인
+                </Button>
+
+            </Form>
+
         </div>
     )
 }
